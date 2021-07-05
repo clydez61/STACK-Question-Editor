761eeb4b861e8d73ab59d729d312accead6d1863
Arthur Wong <arthurawong55@gmail.com>
2021-07-05 14:53:00 -0600

    merge arthur-dev to main and fix detecting input

--- STACK_QT5/stack_properties_box.py
+++ STACK_QT5/stack_properties_box.py
@@ -12,20 +12,25 @@ from math import ceil
 class PropertiesBox(QStackedWidget):
     treeDataSignal = pyqtSignal(dict)
     nodeDataSignal = pyqtSignal(dict)
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.initUI()
 
     def initUI(self):
         noSubWindowLayout = QVBoxLayout()
-        noSubWindowLayout.setAlignment(Qt.AlignTop)
         noneSelectedLayout = QVBoxLayout()
         nodeSelectedLayout = QVBoxLayout()
 
         noSubWindowText = QLabel("A potential response tree has not been selected yet!")
         noSubWindowText.setWordWrap(True)
+        noSubWindowText.setAlignment(Qt.AlignTop)
         noSubWindowLayout.addWidget(noSubWindowText)
 
+        noneSelectedLayout.addWidget(QLabel("Tree Name"))
+        self.treeName = QLineEdit()
+        self.treeName.textChanged.connect(self.transmitTreeData)
+        noneSelectedLayout.addWidget(self.treeName)
         noneSelectedLayout.addWidget(QLabel("Potential Repsone Tree Mark:"))
         self.PRTValue = QLineEdit()
         self.PRTValue.textChanged.connect(self.transmitTreeData)
@@ -106,11 +111,13 @@ class PropertiesBox(QStackedWidget):
 
     def transmitTreeData(self):
         transmit = {}
+        transmit['treeName'] = self.treeName.text()
         transmit['PRTValue'] = self.PRTValue.text()
         transmit['feedbackVar'] = self.feedbackVar.toPlainText()
         self.treeDataSignal.emit(transmit)
 
     def displayTreeData(self, data):
+        self.treeName.setText(data['treeName'])
         self.PRTValue.setText(data['PRTValue'])
         self.feedbackVar.setPlainText(data['feedbackVar'])
 
--- STACK_QT5/stack_window.py
+++ STACK_QT5/stack_window.py
@@ -100,6 +100,7 @@ class StackWindow(NodeEditorWindow):
     def onFileNew(self):
         try:
             subwnd = self.createMdiChild()
+            subwnd.showMaximized()
             subwnd.widget().fileNew()
             subwnd.show()
         except Exception as e: dumpException(e)
@@ -214,18 +215,15 @@ class StackWindow(NodeEditorWindow):
         #     self.windowMapper.setMapping(action, window)
 
     def mouseReleaseEvent(self, event):
-        print("Test") 
         self.updateEditorPropertiesBox()
         super().mouseReleaseEvent(event)
 
     def updateEditorPropertiesBox(self):
         currentSubWnd = self.getCurrentNodeEditorWidget()
         if currentSubWnd is None:
-            self.propertiesWidgetScroll.setWidgetResizable(False)
             self.propertiesWidget.setNoSubWindowLayout()
         else:
             self.propertiesWidget.treeDataSignal.connect(self.storeTreeData)
-            self.propertiesWidgetScroll.setWidgetResizable(True)
             self.displayTreeData()
             if currentSubWnd.hasSelectedItem():
                 self.displayNodeData(self.getNodeData())
@@ -291,7 +289,6 @@ class StackWindow(NodeEditorWindow):
         self.propertiesWidget.nodeDataSignal.connect(self.storeNodeData)
 
         self.propertiesWidgetScroll.setWidget(self.propertiesWidget)
-        self.propertiesWidgetScroll.setWidgetResizable(False)
         self.propertiesDock = QDockWidget("Properties")
         self.propertiesDock.setWidget(self.propertiesWidgetScroll)
         self.propertiesDock.setFloating(False)
@@ -310,11 +307,24 @@ class StackWindow(NodeEditorWindow):
         # nodeeditor.scene.addItemSelectedListener(self.updateEditMenu)
         # nodeeditor.scene.addItemsDeselectedListener(self.updateEditMenu)
         # nodeeditor.scene.history.addHistoryModifiedListener(self.updateEditMenu)
+        nodeeditor.treeName = self.getNewSubWindowName()
         nodeeditor.scene.history.addHistoryModifiedListener(self.updateEditorPropertiesBox)
         nodeeditor.scene.history.addHistoryModifiedListener(self.nodeEditorModified.emit)
         nodeeditor.nodeDataModified.connect(self.displayNodeData)
         nodeeditor.addCloseEventListener(self.onSubWndClose)
         return subwnd
+
+    def getNewSubWindowName(self):
+        name = "New Tree"
+        if self.findChildViaTreeName(name) is None:
+            return name
+        
+        i = 1
+        while True:
+            name = "New Tree - " + str(i)
+            if self.findChildViaTreeName(name) is None:
+                return name
+            i = i+1
 
     def onSubWndClose(self, widget, event):
         existing = self.findMdiChild(widget.filename)
@@ -331,6 +341,12 @@ class StackWindow(NodeEditorWindow):
                 return window
         return None
 
+    def findChildViaTreeName(self, name):
+        for window in self.mdiArea.subWindowList():
+            if window.widget().treeName == name:
+                return window
+        return None
+
     def setActiveSubWindow(self, window):
         if window:
             self.mdiArea.setActiveSubWindow(window)
@@ -344,4 +360,7 @@ class StackWindow(NodeEditorWindow):
     def deserialize(self, data, hashmap=[]):
         for subWndData in data:
             subwindow = self.createMdiChild()
-            subwindow.deserialize(subWndData)
+            subwindow.showMaximized()
+            subwindow.widget().deserialize(subWndData)
+            subwindow.widget().scene.history.clear()
+            subwindow.widget().has_been_modified = False
